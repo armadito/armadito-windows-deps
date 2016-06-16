4e73246b8381df7fb42ed8afaf9f899cd990a18f
François Déchelle <fdechelle@teclib.com>
2016-06-16 19:02:07 +0200

    updated json-c to build in all versions (32 and 64, debug and release); scripted the build

--- json-c/makedist.bat
+++ json-c/makedist.bat
@@ -1,16 +1,22 @@
 
-msbuild json-c-master-2016-04-21\json-c.vcxproj /t:rebuild /p:Configuration=Debug
-call :copyOne Debug
-msbuild json-c-master-2016-04-21\json-c.vcxproj /t:rebuild /p:Configuration=Release
-call :copyOne Release
+call :buildAndCopy Win32 Debug
+call :buildAndCopy Win32 Release
+call :buildAndCopy x64 Debug
+call :buildAndCopy x64 Release
 goto :eof
 
-:copyOne
+:buildAndCopy
 setlocal
-set configuration=%~1
-if not exist %configuration%\include mkdir %configuration%\include
-xcopy json-c-master-2016-04-21\*.h %configuration%\include /D /I /Y
-if not exist %configuration%\lib mkdir %configuration%\lib
-xcopy json-c-master-2016-04-21\Debug\json-c.lib %configuration%\lib /D /I /Y
+set platform=%~1
+set configuration=%~2
+msbuild json-c-master-2016-04-21\json-c.vcxproj /t:rebuild /p:Platform=%platform% /p:Configuration=%configuration%
+if not exist %platform%\%configuration%\include mkdir %platform%\%configuration%\include
+xcopy json-c-master-2016-04-21\*.h %platform%\%configuration%\include /D /I /Y
+if not exist %platform%\%configuration%\lib mkdir %platform%\%configuration%\lib
+if %platform% == Win32 (
+	xcopy json-c-master-2016-04-21\%configuration%\json-c.lib %platform%\%configuration%\lib /D /I /Y 
+) else (
+	xcopy json-c-master-2016-04-21\%platform%\%configuration%\json-c.lib %platform%\%configuration%\lib /D /I /Y 
+)
 endlocal
 goto :eof
