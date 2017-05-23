7dcda25981160ae17597e522603491b4ffdffcbf
François Déchelle <fdechelle@teclib.com>
2017-05-23 10:51:48 +0200

    Update build script

--- jansson/makedist.bat
+++ jansson/makedist.bat
@@ -10,11 +10,10 @@ set configuration=%~2
 
 msbuild jansson\jansson.vcxproj /t:rebuild /p:Platform=%platform% /p:Configuration=%configuration%
 
-rem if not exist %platform%\%configuration%\include mkdir %platform%\%configuration%\include
-rem xcopy json-c-master-2016-04-21\*.h %platform%\%configuration%\include /D /I /Y
+if not exist %platform%\%configuration%\include mkdir %platform%\%configuration%\include
+xcopy jansson\include\*.h %platform%\%configuration%\include /D /I /Y
 
 if not exist %platform%\%configuration%\lib mkdir %platform%\%configuration%\lib
-
 if %configuration% == Debug (
    copy jansson\lib\%configuration%\jansson_d.lib %platform%\%configuration%\lib\jansson.lib
 ) else (
