be1fed739262b600d6372bf8914accf9a8649507
François Déchelle <fdechelle@teclib.com>
2017-06-07 18:39:24 +0200

    Add glib compilation deps for VS14

--- glib/makedist.bat
+++ glib/makedist.bat
@@ -1,18 +1,24 @@
-msbuild glib-2.47.92\build\win32\vs12\glib.sln /t:rebuild /p:Configuration=Debug_BundledPCRE
-call :copyOne Debug
-msbuild glib-2.47.92\build\win32\vs12\glib.sln /t:rebuild /p:Configuration=Release_BundledPCRE
-call :copyOne Release
+
+call :buildAndCopy x64 Debug Debug_BundledPCRE
+call :buildAndCopy x64 Release Release_BundledPCRE
 
 goto :eof
 
-:copyOne
+:buildAndCopy
 setlocal
-set configuration=%~1
-if not exist vs12\Win32\%configuration% mkdir Win32\%configuration%
-xcopy vs12\Win32\bin Win32\%configuration%\bin /D /S /I /Y
-xcopy vs12\Win32\lib Win32\%configuration%\lib /D /S /I /Y
-xcopy vs12\Win32\include Win32\%configuration%\include /D /S /I /Y
-xcopy vs12\Win32\manifest Win32\%configuration%\manifest /D /S /I /Y
-xcopy vs12\Win32\share Win32\%configuration%\share /D /S /I /Y
+set platform=%~1
+set configuration=%~2
+set pcre_configuration=%~3
+
+set msvs=vs14
+
+msbuild glib-2.47.92\build\win32\%msvs%\glib.sln /t:rebuild /p:Platform=%platform% /p:Configuration=%pcre_configuration%
+
+if not exist %msvs%\%platform%\%configuration% mkdir %platform%\%configuration%
+xcopy %msvs%\%platform%\bin %platform%\%configuration%\bin /D /S /I /Y
+xcopy %msvs%\%platform%\lib %platform%\%configuration%\lib /D /S /I /Y
+xcopy %msvs%\%platform%\include %platform%\%configuration%\include /D /S /I /Y
+xcopy %msvs%\%platform%\manifest %platform%\%configuration%\manifest /D /S /I /Y
+xcopy %msvs%\%platform%\share %platform%\%configuration%\share /D /S /I /Y
 endlocal
 goto :eof
